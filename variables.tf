cdda7f38bb5843cea6b606aa2783aecd51b783e8
Boris Yakimov <boris.yakimov@itgix.com>
2024-08-19 13:31:05 +0300

    #65 test to add provider for cross acc in module

--- variables.tf
+++ variables.tf
@@ -1,4 +1,7 @@
+# TODO: add details and type safety
 variable "wildcard_cert" {}
 variable "zone_name" {}
 variable "hostname" {}
 variable "validation_record_fqdns" {}
+variable "shared_services_iam_role" {}
+variable "aws_region" {}
